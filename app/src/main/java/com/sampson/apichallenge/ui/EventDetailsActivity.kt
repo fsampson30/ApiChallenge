45c2c4036f40bc51eefc9d3aee3aa1d762f3cd12
fsampson30 <fsampson30@gmail.com>
2022-05-19 19:44:24 -0300

    UI changes on EventDetailsActivity.

--- app/src/main/java/com/sampson/apichallenge/ui/EventDetailsActivity.kt
+++ app/src/main/java/com/sampson/apichallenge/ui/EventDetailsActivity.kt
@@ -1,7 +1,10 @@
 package com.sampson.apichallenge.ui
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import com.sampson.apichallenge.R
@@ -16,13 +19,25 @@ class EventDetailsActivity : AppCompatActivity() {
         val imgEvent: ImageView = findViewById(R.id.imgDetailsActivity)
         val txtTitle: TextView = findViewById(R.id.txtEventDetailsTitle)
         val txtDescription: TextView = findViewById(R.id.txtEventDetailsDescription)
+        val txtPrice: TextView = findViewById(R.id.txtEventDetailsPrice)
+        val txtLocation: TextView = findViewById(R.id.txtEventDetailsLocation)
 
         val event = intent.getParcelableExtra<Events>("event")
 
         txtTitle.text = event?.title
         txtDescription.text = event?.description
+        "Preço R$${event?.price.toString()}".also { txtPrice.text = it }
 
-        Picasso.get().load(event?.image).into(imgEvent)
+        Picasso.get().load(event?.image).fetch()
+        Picasso.get().load(event?.image).placeholder(R.mipmap.ic_launcher).into(imgEvent)
+
+        txtLocation.setOnClickListener {
+            val intentUri = Uri.parse("geo:${event?.latitude},${event?.longitude}")
+            val mapIntent = Intent(Intent.ACTION_VIEW, intentUri).apply {
+                setPackage("com.google.android.apps.maps")
+            }
+            startActivity(mapIntent)
+        }
 
 
     }
